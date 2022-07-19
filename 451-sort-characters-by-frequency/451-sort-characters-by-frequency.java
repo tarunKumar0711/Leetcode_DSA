c089b7cb5036521ea621558ed0ee6da5740ce472
Tarun Kumar <68737903+tathaastu-tarun@users.noreply.github.com>
2022-07-19 21:20:16 +0530

    Time: 2561 ms (5.04%), Space: 670.6 MB (5.01%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,32 +1,22 @@
 class Solution {
-    class Pair{
-        char ch;
-        int n;
-        public Pair(char ch , int n){
-            this.ch = ch;
-            this.n = n;
-        }
-    }
+  
     public String frequencySort(String s) {
         Map<Character,Integer> map = new HashMap<>();
-        for(int i =0;i<s.length();i++)
-                map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+    for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }  
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->b.n-a.n);
+        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> map.get(b)-map.get(a));
         
-        for(Map.Entry<Character,Integer> mapEle: map.entrySet())
-        {
-            char key = mapEle.getKey();
-            int value = mapEle.getValue();
-            pq.offer(new Pair(key,value));
+       for (char c : map.keySet()) {
+            pq.offer(c);
         }
         String ans="";
         
         while(pq.size()>0)
         {
-            Pair p = pq.poll();
-            char c = p.ch;
-            int q = p.n;
+            char c = pq.poll();
+            int q = map.get(c);
             while(q-->0)
                 ans+=c;
         }
