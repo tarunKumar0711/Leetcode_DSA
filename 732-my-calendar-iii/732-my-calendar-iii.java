fd418bf741e39aa258f4de6a4cd081fe891c836d
Tarun Kumar <68737903+tathaastu-tarun@users.noreply.github.com>
2022-10-07 20:06:46 +0530

    Time: 52 ms (85.75%), Space: 54.9 MB (35.87%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.java
@@ -0,0 +1,26 @@
+class MyCalendarThree {
+    TreeMap<Integer, Integer> count;
+    int max;
+    public MyCalendarThree() {
+        count = new TreeMap();
+        count.put(-1, 0);
+        max = 0;
+    }
+    
+    public int book(int start, int end) {
+        count.put(start, count.floorEntry(start).getValue());
+        count.put(end, count.floorEntry(end).getValue());
+        for(Map.Entry<Integer, Integer> entry : count.subMap(start, end).entrySet()){
+            int val = entry.getValue() + 1;
+            entry.setValue(val);
+            max = Math.max(max, val);
+        }
+        return max;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ */
